0920760b86fb31df17504faf36da6c37621ad242
kauagrathwohl <kauagrathwohl095@gmail.com>
2024-11-23 15:04:41 -0300

    Corrigindo estilização tela de cadastro de receitas

--- src/pages/receita/Detalhes.jsx
+++ src/pages/receita/Detalhes.jsx
@@ -27,18 +27,18 @@ export default function Detalhes({ id, onClose, children }) {
     request(`/receita/${id}`, {
       method: 'GET',
     })
-      .then(({ ingredientes, ...data }) => {
-        setLoading(false);
-        setIngredientes(ingredientes || []);
-        form.setFieldsValue(data);
-      })
-      .catch((err) => {
-        setLoading(false);
-        Modal.error({
-          title: 'Erro!',
-          content: err,
+        .then(({ ingredientes, ...data }) => {
+          setLoading(false);
+          setIngredientes(ingredientes || []);
+          form.setFieldsValue(data);
+        })
+        .catch((err) => {
+          setLoading(false);
+          Modal.error({
+            title: 'Erro!',
+            content: err,
+          });
         });
-      });
   };
 
   const handleSubmit = (values) => {
@@ -51,18 +51,18 @@ export default function Detalhes({ id, onClose, children }) {
       method: id ? 'PUT' : 'POST',
       body: { ...values, ingredientes, id },
     })
-      .then(() => {
-        setLoading(false);
-        handleClear();
-        onClose?.();
-      })
-      .catch((err) => {
-        setLoading(false);
-        Modal.error({
-          title: 'Erro!',
-          content: err,
+        .then(() => {
+          setLoading(false);
+          handleClear();
+          onClose?.();
+        })
+        .catch((err) => {
+          setLoading(false);
+          Modal.error({
+            title: 'Erro!',
+            content: err,
+          });
         });
-      });
   };
 
   const handleClear = () => {
@@ -113,45 +113,61 @@ export default function Detalhes({ id, onClose, children }) {
         'Content-Type': 'application/json',
       },
     })
-      .then((response) => {
-        setLoading(false);
+        .then((response) => {
+          setLoading(false);
 
-        const updatedFields = {
-          proteinas: response.macros.proteinas,
-          gorduras: response.macros.gorduras,
-          carboidratos: response.macros.carboidratos,
-          calorias: response.macros.calorias,
-        };
+          const updatedFields = {
+            proteinas: response.macros.proteinas,
+            gorduras: response.macros.gorduras,
+            carboidratos: response.macros.carboidratos,
+            calorias: response.macros.calorias,
+          };
 
-        if (gerarModoPreparoIA) {
-          updatedFields.preparo = response.preparo;
-          updatedFields.gramas = response.gramasPorPorcao;
-        }
-        form.setFieldsValue(updatedFields);
-      })
-      .catch((error) => {
-        setLoading(false);
-        Modal.error({
-          title: 'Erro ao calcular os dados',
-          content: 'Ocorreu um erro ao processar a receita. Tente novamente.',
+          if (gerarModoPreparoIA) {
+            updatedFields.preparo = response.preparo;
+            updatedFields.gramas = response.gramasPorPorcao;
+          }
+          form.setFieldsValue(updatedFields);
+        })
+        .catch((error) => {
+          setLoading(false);
+          Modal.error({
+            title: 'Erro ao calcular os dados',
+            content: 'Ocorreu um erro ao processar a receita. Tente novamente.',
+          });
         });
-      });
   };
 
   return (
-    <span>
-      <span onClick={modal}
-        style={{ cursor: 'pointer' }}>
+      <span>
+      <span onClick={modal} style={{ cursor: 'pointer' }}>
         {children}
       </span>
-      <Modal open={visible}
-        title="Cadastro de Receita"
-        okText="Salvar"
-        centered
-        destroyOnClose
-        onCancel={handleClear}
-        onOk={form.submit}
-        width={850}>
+      <Modal
+          open={visible}
+          title="Cadastro de Receita"
+          okText="Salvar"
+          centered
+          destroyOnClose
+          onCancel={handleClear}
+          onOk={form.submit}
+          width={850}
+          footer={[
+            <Row justify="space-between" style={{ width: '100%' }}>
+              <Button key="calculoIA" type="default" onClick={handleCalculoIA}>
+                Cálculo IA
+              </Button>
+              <div>
+                <Button key="cancel" onClick={handleClear} style={{ marginRight: 20 }}>
+                  Cancelar
+                </Button>
+                <Button key="submit" type="primary" onClick={form.submit}>
+                  Salvar
+                </Button>
+              </div>
+            </Row>,
+          ]}
+      >
         <Form form={form} layout="vertical" onFinish={handleSubmit}>
           <Spin spinning={loading}>
             <Row gutter={[10, 5]} justify="center">
@@ -162,74 +178,75 @@ export default function Detalhes({ id, onClose, children }) {
               </Col>
 
               {ingredientes.map((el, i) => (
-                <Col key={i} span={24}>
-                  <Row gutter={[10, 5]}>
-                    <Col span={14}>
-                      <Input
-                        placeholder="Ingrediente"
-                        value={el.ingrediente}
-                        onChange={(e) =>
-                          changeIngrediente(e.target.value, 'ingrediente', i)
-                        }
-                      />
-                    </Col>
-                    <Col span={5}>
-                      <InputNumber
-                        placeholder="Qtd."
-                        value={el.quantidade}
-                        style={{ width: '100%' }}
-                        onChange={(value) =>
-                          changeIngrediente(value, 'quantidade', i)
-                        }
-                      />
-                    </Col>
-                    <Col span={4}>
-                      <Input
-                        placeholder="UN"
-                        maxLength={2}
-                        value={el.unidade}
-                        onChange={(e) =>
-                          changeIngrediente(e.target.value, 'un', i)
-                        }
-                      />
-                    </Col>
-                    <Col span={1}>
-                      <Button
-                        danger
-                        icon={<DeleteOutlined />}
-                        onClick={() => removeIngrediente(i)}
-                      />
-                    </Col>
-                  </Row>
-                </Col>
+                  <Col key={i} span={24}>
+                    <Row gutter={[10, 5]}>
+                      <Col span={14}>
+                        <Input
+                            placeholder="Ingrediente"
+                            value={el.ingrediente}
+                            onChange={(e) =>
+                                changeIngrediente(e.target.value, 'ingrediente', i)
+                            }
+                        />
+                      </Col>
+                      <Col span={5}>
+                        <InputNumber
+                            placeholder="Qtd."
+                            value={el.quantidade}
+                            style={{ width: '100%' }}
+                            onChange={(value) =>
+                                changeIngrediente(value, 'quantidade', i)
+                            }
+                        />
+                      </Col>
+                      <Col span={4}>
+                        <Input
+                            placeholder="UN"
+                            maxLength={2}
+                            value={el.unidade}
+                            onChange={(e) =>
+                                changeIngrediente(e.target.value, 'un', i)
+                            }
+                        />
+                      </Col>
+                      <Col span={1}>
+                        <Button
+                            danger
+                            icon={<DeleteOutlined />}
+                            onClick={() => removeIngrediente(i)}
+                        />
+                      </Col>
+                    </Row>
+                  </Col>
               ))}
 
               <Col style={{ marginTop: 10 }}>
                 <Button onClick={addIngrediente}>Adicionar Ingrediente</Button>
               </Col>
 
-              <Col span={24} style={{ marginTop: 10, textAlign: 'left' }}>
-                <Checkbox
-                  checked={gerarModoPreparoIA}
-                  onChange={(e) => setGerarModoPreparoIA(e.target.checked)}
-                >
-                  Gerar modo de preparo por IA
-                </Checkbox>
+              <Col span={24} style={{ marginTop: 10 }}>
+                <Row justify="space-between" align="middle">
+                  <Col style={{ fontSize: 20, fontWeight: 'bold', opacity: 0.8 }}>
+                    Modo de Preparo
+                  </Col>
+                  <Col>
+                    <Checkbox
+                        checked={gerarModoPreparoIA}
+                        onChange={(e) => setGerarModoPreparoIA(e.target.checked)}
+                    >
+                      Gerar modo de preparo por IA
+                    </Checkbox>
+                  </Col>
+                </Row>
               </Col>
 
-              <Col
-                span={24}
-                style={{ fontSize: 20, fontWeight: 'bold', opacity: 0.8 }}
-              >
-                Modo de Preparo
-              </Col>
               <Col span={24}>
                 <Form.Item name="preparo">
                   <Input.TextArea rows={10} />
                 </Form.Item>
               </Col>
 
-              <Row gutter={[21, 21]} justify="space-between">
+              <Row gutter={[21, 10]} justify="space-between">
                 <Col span={6}>
                   <Form.Item name="proteinas" label="Proteínas (g)">
                     <InputNumber min={0} precision={2} style={{ width: '100%' }} />
@@ -250,22 +267,12 @@ export default function Detalhes({ id, onClose, children }) {
                     <InputNumber min={0} precision={2} style={{ width: '100%' }} />
                   </Form.Item>
                 </Col>
+                <Col span={6}>
+                  <Form.Item name="gramas" label="Gramas por Porção (g)">
+                    <InputNumber min={0} precision={2} style={{ width: '100%' }} />
+                  </Form.Item>
+                </Col>
               </Row>
-
-              <Col span={24} style={{ marginTop: 10 }}>
-                <Form.Item name="gramas" label="Gramas por Porção (g)">
-                  <InputNumber min={0} precision={2} style={{ width: '20%' }} />
-                </Form.Item>
-              </Col>
-
-              <Col span={24} style={{ marginTop: 10 }}>
-                <Button
-                  type="default"
-                  style={{ width: '100px' }}
-                  onClick={handleCalculoIA}>
-                  Cálculo IA
-                </Button>
-              </Col>
             </Row>
           </Spin>
         </Form>
